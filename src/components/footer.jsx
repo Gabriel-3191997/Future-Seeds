d2f6912b755149a0abae73c6b685d825522e234f
Gabriel W. Kun <gabrielwkun@gmail.com>
2026-05-29 19:23:36 +0100

    deployment

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,22 +5,22 @@ class Footer extends React.Component {
         return (
             <>
                 <footer className="bg-white rounded-none border-none font-serif m-4 reveal-up reveal-delay-5">
-                    <div className="w-full mx-auto max-w-screen-xl p-4 flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
+                    <div className="w-full mx-auto max-w-7xl p-4 flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
                         <span className="text-sm text-body text-center md:text-left">
-                            © 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+                            © 2026 <a href="https://flowbite.com/" className="hover:underline">Future Seeds</a>. All Rights Reserved.
                         </span>
                         <ul className="flex flex-wrap items-center justify-center gap-x-4 gap-y-2 text-sm sm:text-lg font-medium text-body md:justify-end">
                             <li>
                                 <a href="#" className="hover:underline">About</a>
                             </li>
                             <li>
-                                <a href="#" className="hover:underline">Privacy Policy</a>
+                                <a href="#" className="hover:underline">LegalPolicy</a>
                             </li>
                             <li>
-                                <a href="#" className="hover:underline">Licensing</a>
+                                <a href="#" className="hover:underline">Terms &amp; Conditions</a>
                             </li>
                             <li>
-                                <a href="#" className="hover:underline">Contact</a>
+                                <a href="#" className="hover:underline">Scholarships</a>
                             </li>
                         </ul>
                     </div>
